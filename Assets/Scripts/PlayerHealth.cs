279a02d2966ef5a2151ed429087765c43be5b0d0
RoyalWeden <roy.weden@yahoo.com>
2020-03-24 11:34:10 -0500

    added player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public float health = 2;
+    public Transform spawnPoint;
+    public Transform ground;
+
+    private void Start() {
+        health = 2;
+    }
+
+    void Update() {
+        if(health<=0) {
+            transform.position = spawnPoint.position;
+            health = 2;
+        }
+        if(transform.position.y<ground.position.y-10) {
+            health = 0;
+        }
+    }
+
+    public void hurtPlayer() {
+        health--;
+    }
+}

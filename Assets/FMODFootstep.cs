875f4fdcf446af416490163dc49a74b714b950e4
SoundsByDane <danereid1024@icloud.com>
2020-03-24 15:37:26 -0400

    footsteps sounds updated

---
+++ Assets/FMODFootstep.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODFootstep : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string inputsound;
+    bool isRunning;
+    public float playerSpeed;
+
+    void Update ()
+	{
+        if (Input.GetAxis ("Vertical") >= 0.01f || Input.GetAxis ("Horizontal") >= 0.01f || Input.GetAxis ("Vertical") <= -0.01f || Input.GetAxis ("Horizontal") <= -0.01f)
+		{
+            isRunning = true;
+
+        }
+        else if (Input.GetAxis ("Vertical") == 0 || Input.GetAxis ("Horizontal") == 0)
+		{
+            isRunning = false;
+        }
+  }
+    void CallFootsteps ()
+	{
+        if (isRunning == true)
+		{
+            FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+        }
+	}
+    void Start ()
+	{
+        InvokeRepeating("CallFootsteps", 0, playerSpeed);
+	}
+
+    void onDisable ()
+	{
+        isRunning = false;
+    }
+}
+

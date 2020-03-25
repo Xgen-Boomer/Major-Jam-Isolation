661303a53a077ebd9482d9d0a7448249db837193
RoyalWeden <roy.weden@yahoo.com>
2020-03-25 18:45:42 -0500

    added Death sound code

---
+++ Assets/FMODDeath.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMODDeath : MonoBehaviour {
+    [FMODUnity.EventRef]
+    public string inputsound;
+    bool isDead;
+    public float playerSpeed;
+
+    void Update() {
+        if(GetComponent<PlayerHealth>().health==0) {
+            isDead = true;
+        } else {
+            isDead = false;
+        }
+    }
+    void CallFootsteps() {
+        if (isDead) {
+            FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+        }
+    }
+    void Start() {
+        playerSpeed = .5f;
+        isDead = false;
+        InvokeRepeating("CallFootsteps", 0, .5f);
+    }
+
+    void onDisable() {
+        isDead = false;
+    }
+}
+

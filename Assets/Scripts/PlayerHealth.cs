c4e72e23ba9ade2ca7dd12f97ce7838434bcea9e
RoyalWeden <roy.weden@yahoo.com>
2020-03-26 08:40:11 -0500

    updated ui script

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,21 +9,21 @@ public class PlayerHealth : MonoBehaviour {
     public Transform ground;
     public GameObject deathPanel;
     bool spawned = true;
+    public Animator deathPanelAnimator;
 
     public string deathType = "";
 
     private void Start() {
         spawnPoint = new Vector3(transform.position.x, transform.position.y, transform.localScale.x);
-        Debug.Log(spawnPoint);
-        deathPanel = GameObject.Find("DeathPanel");
         health = 2;
     }
 
     void Update() {
         if(health<=0) {
             if(spawned) {
-                StartCoroutine(playDeathSound());
                 deathPanel.SetActive(true);
+                deathPanelAnimator.SetBool("Dead", true);
+                StartCoroutine(playDeathSound());
                 spawned = false;
             }
         }
@@ -39,10 +39,11 @@ public class PlayerHealth : MonoBehaviour {
 
     IEnumerator playDeathSound() {
         GetComponent<FMODDeath>().isDead = true;
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(.85f);
     }
 
     public void resetPlayer() {
+        deathPanelAnimator.SetBool("Dead", false);
         health = 2;
         transform.position = new Vector2(spawnPoint.x, spawnPoint.y);
         transform.localScale = new Vector2(spawnPoint.z, spawnPoint.z);

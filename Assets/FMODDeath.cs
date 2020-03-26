4db2b74045bb04e9a23eee532d11596ba2136232
RoyalWeden <roy.weden@yahoo.com>
2020-03-25 20:44:50 -0500

    updated death code

--- Assets/FMODDeath.cs
+++ Assets/FMODDeath.cs
@@ -5,25 +5,22 @@ using UnityEngine;
 public class FMODDeath : MonoBehaviour {
     [FMODUnity.EventRef]
     public string inputsound;
-    bool isDead;
+    public bool isDead;
     public float playerSpeed;
 
     void Update() {
-        if(GetComponent<PlayerHealth>().health==0) {
-            isDead = true;
-        } else {
-            isDead = false;
-        }
+        
     }
     void CallFootsteps() {
         if (isDead) {
             FMODUnity.RuntimeManager.PlayOneShot(inputsound);
+            isDead = false;
         }
     }
     void Start() {
         playerSpeed = .5f;
         isDead = false;
-        InvokeRepeating("CallFootsteps", 0, .5f);
+        InvokeRepeating("CallFootsteps", 0, playerSpeed);
     }
 
     void onDisable() {
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour {
 
     public float health = 2;
-    public Transform spawnPoint;
+    public GameObject spawnPoint;
     public Transform ground;
+    public GameObject deathPanel;
+    bool spawned = true;
+
+    public string deathType = "";
 
     private void Start() {
+        deathPanel = GameObject.Find("DeathPanel");
+        spawnPoint = GameObject.Find("PlayerSpawnPoint");
         health = 2;
     }
 
     void Update() {
         if(health<=0) {
-            transform.position = spawnPoint.position;
-            health = 2;
+            if(spawned) {
+                StartCoroutine(playDeathSound());
+                spawned = false;
+            }
         }
         if(transform.position.y<ground.position.y-10) {
             health = 0;
+            deathType = "Fell into the abyss.";
         }
     }
 
     public void hurtPlayer() {
         health--;
+    }
+
+    IEnumerator playDeathSound() {
+        GetComponent<FMODDeath>().isDead = true;
+        yield return new WaitForSeconds(1.5f);
+    }
+
+    public void resetPlayer() {
+        deathType = "";
+        transform.position = spawnPoint.transform.position;
+        health = 2;
+        spawned = true;
     }
 }
